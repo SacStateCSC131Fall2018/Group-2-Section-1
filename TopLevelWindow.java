fb78fb13a5075d1fa3b33875cb0fbb9ad25f134f
Tyler Fox <tylerfox@csus.edu>
2018-12-02 17:27:22 -0800

    Made main window

---
+++ TopLevelWindow.java
@@ -0,0 +1,24 @@
+ import java.awt.*;
+import javax.swing.*;
+
+public class TopLevelWindow {
+   private static void createWindow() {
+      //Create and set up the window.
+      JFrame frame = new JFrame("Pirex");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      //JLabel textLabel = new JLabel("I'm a label in the window",SwingConstants.CENTER);
+      //textLabel.setPreferredSize(new Dimension(300, 100));
+      //frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+      
+      //Display the window.
+      frame.setLocationRelativeTo(null);
+      frame.setPreferredSize(new Dimension(700, 400));
+      frame.pack();
+      frame.setVisible(true);
+   }
+   
+   public static void main(String[] args) {
+      createWindow();
+   }
+} 
