3f3ad7c3e9e7b300a9b3780830fd94a66b744b68
Julissa Bonilla <julissabonilla@csus.edu>
2018-12-04 16:38:21 -0800

    Add files via upload

---
+++ load_summaryPart/src/load_summaryPart/processDriver.java
@@ -0,0 +1,23 @@
+/*
+ * 	 * An example that uses a menu bar
+	 *
+	 */   package load_summaryPart;
+	public class processDriver{
+  
+  //   import load_summaryPart.process;
+
+	
+	      /**he entry point of the example
+	     *
+	     * @param args   The command line arguments
+	     */
+	    public static void main(String[] args)
+	    {
+	        process   f;
+
+	        f = new process();
+	        f.setSize(400,200);
+	        f.setVisible(true);
+
+	    }
+	}
