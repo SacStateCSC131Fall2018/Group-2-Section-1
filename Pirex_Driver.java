4a99bf429ac28a331e9af0575e4ae03ff6fa2f55
Anand rai <anandrai@csus.edu>
2018-12-05 13:17:28 -0800

    Created the TabbedPanes object in the main driver

--- Pirex_Driver.java
+++ Pirex_Driver.java
@@ -4,7 +4,13 @@ public class Pirex_Driver {
 		//TopLevelWindow.createWindow();
       TopLevelWindow main = new TopLevelWindow();
       
-      //
+      //create the tabs 
+      TabbedPane tabs = new TabbedPane();
+      //LoadPanel load = new LoadPanel();
+      //tabs.add("Load Documents", load, "Load documents to our Pirex");
+      
+      //add the tabs to our main window
+      main.add(tabs);
       
 		
 	}
