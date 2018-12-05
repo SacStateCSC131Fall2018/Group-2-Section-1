9aaf69ab40d5221c11873d816d2914b8d0318707
Anand rai <anandrai@csus.edu>
2018-12-05 12:53:44 -0800

    Revert "changed AddPanel to only accept Strings"
    This reverts commit 8277aee82d127caf21e6e02350fda105c90a5d47.

--- TabbedPane.java
+++ TabbedPane.java
@@ -7,10 +7,16 @@ public class TabbedPane extends JTabbedPane{
    	//this.tabbedPane = new JTabbedPane();
    }
    
+<<<<<<< HEAD
    public void AddPanel(String name, String description){
       
       JPanel panel = new JPanel();
       this.addTab(name,  null, panel, description);
       this.setSelectedIndex(0);
+=======
+   public void AddPanel(String name, JPanel panel, String description){
+      this.tabbedPane.addTab(name,  null, panel, description);
+      this.tabbedPane.setSelectedIndex(0);
+>>>>>>> parent of 8277aee... changed AddPanel to only accept Strings
    }
 }
