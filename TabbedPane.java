38e586a6da33397188285844135f56b2ad79b965
Anand rai <anandrai@csus.edu>
2018-12-05 12:51:20 -0800

    Changed Constructor so it extends rather than has

--- TabbedPane.java
+++ TabbedPane.java
@@ -1,16 +1,16 @@
 import java.awt.*;
 import javax.swing.*;
-public class TabbedPane{
-   JTabbedPane tabbedPane;
-   public void TabbedPane(){
-      
-   	this.tabbedPane = new JTabbedPane();
+public class TabbedPane extends JTabbedPane{
+   //JTabbedPane tabbedPane;
+   public TabbedPane(){
+      super();
+   	//this.tabbedPane = new JTabbedPane();
    }
    
    public void AddPanel(String name, String description){
       
       JPanel panel = new JPanel();
-      this.tabbedPane.addTab(name,  null, panel, description);
-      this.tabbedPane.setSelectedIndex(0);
+      this.addTab(name,  null, panel, description);
+      this.setSelectedIndex(0);
    }
 }
