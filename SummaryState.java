ec31cdc3c17507380ad118df9224fbe042886c5a
jasonw67 <jasongwong@csus.edu>
2018-12-06 12:33:38 -0800

    Fixing bug to make panel resizable

--- SummaryState.java
+++ SummaryState.java
@@ -9,25 +9,18 @@ public class SummaryState extends JPanel{
    }
    //Layout
    public void setLayout(){
-      /*
-      setSize(900,500);
-      setVisible(true);
-      Container c = getContentPane();
-      */
-      this.setLayout(new FlowLayout());
+      setLayout(new GridLayout());
       JLabel textBox = new JLabel("");
       textBox.setVerticalAlignment(JLabel.TOP);
       JScrollPane scroll = new JScrollPane(textBox);
-      scroll.setPreferredSize(new Dimension(800,400));
       scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-      JTabbedPane tab = new JTabbedPane();
-      tab.addTab("Summarize Document",scroll);
-      this.add(tab);
-      //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      add(scroll);
    }
-   
-   /*
    public static void main(String[] args){
       SummaryState start = new SummaryState();
-   }*/
+      JFrame window = new JFrame("Pirex");
+      window.setSize(900,500);
+      window.add(start);
+      window.setVisible(true);
+   }
 }
