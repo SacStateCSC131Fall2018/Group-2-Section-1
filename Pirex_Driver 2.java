edfff57ea2d141d4c16dfbf2adb2e10d7736670c
Anand rai <anandrai@csus.edu>
2018-12-05 16:14:57 -0800

    Import java.swing on driver

--- Pirex_Driver 2.java
+++ Pirex_Driver 2.java
@@ -1,26 +1,25 @@
 import java.awt.*;
 import javax.swing.*;
 
-
 public class Pirex_Driver {
 	public static void main(String[] args)
 	{
 		//TopLevelWindow.createWindow();
-      TopLevelWindow main = new TopLevelWindow();
+      //TopLevelWindow main = new TopLevelWindow();
       
       //create the tabs 
       TabbedPane tabs = new TabbedPane();
       //LoadPanel load = new LoadPanel();
       //tabs.add("Load Documents", load, "Load documents to our Pirex");
       
-      //Creates and adds search panel to tabs
-      SearchForDocument search = new SearchForDocument();
-      tabs.AddPanel("Search for Documents", search, "Search documents on our Pirex");
-      
       //add the tabs to our main window
+      JFrame Test = new JFrame();
+      Test.setVisible(true);
+      
       MenuBar new_menu = new MenuBar();
-      main.add(tabs);
-      main.getContentPane().add(new_menu, BorderLayout.NORTH);
+      //main.add(tabs);
+      Test.getContentPane().add(new_menu);
+      //main.getContentPane().add(new_menu, BorderLayout.NORTH);
       
 		
 	}
