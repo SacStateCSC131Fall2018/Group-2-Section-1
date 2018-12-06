b5fd1a3b6fb19771a8c1c86a81b49feec9e9c237
Roberto Campos <robertocampos@csus.edu>
2018-12-05 16:34:50 -0800

    Adding multiple lines of code

--- LoadPanel.java
+++ LoadPanel.java
@@ -14,6 +14,38 @@ public class LoadPanel extends JPanel
    //Set layout of the load panel
    private void setLayout()
    {
-      
+      JPanel MAINPANEL = new JPanel();  // This panel is the mail panel and holds all the other panels
+		JPanel panel = new JPanel(new GridLayout(3,0)); //this panel creates a grid holds three panels
+		JPanel p1 = new JPanel(new FlowLayout());
+		JPanel p2 = new JPanel(new FlowLayout());
+		JPanel p3 = new JPanel(new FlowLayout());
+		
+		JTextField topTxt = new JTextField("",30);
+		JLabel topLabel = new JLabel("TextFile:");
+		
+		String[] file = {"Project Guten Burge File"};
+		JComboBox types = new JComboBox(file); //Drop down menu
+		JLabel fileType = new JLabel("Text File Type:");
+		
+		
+		JTextField tf = new JTextField("",12);
+		JLabel label = new JLabel("Title:");
+		JTextField tf2 = new JTextField("",12);
+		JLabel label2 = new JLabel("Author:");
+		
+		p1.add(topLabel);
+		p1.add(topTxt);
+		p2.add(fileType);
+		p2.add(types);
+		p3.add(label);
+		p3.add(tf);
+		p3.add(label2);
+		p3.add(tf2);
+		panel.add(p1);
+		panel.add(p2);
+		panel.add(p3);
+		MAINPANEL.add(panel); //added everything to main panel
+		
+		frame.getContentPane().add(MAINPANEL, BorderLayout.NORTH);
    }
 }
--- TopLevelWindow.java
+++ TopLevelWindow.java
@@ -45,5 +45,5 @@ public class TopLevelWindow extends JFrame{
       
    }*/
    
-   //TestingCommit
+ 
 } 
