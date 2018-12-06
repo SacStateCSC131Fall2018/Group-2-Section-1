d4852ddcaefd54118b2c958b3406d6cfe7481a14
Anand rai <anandrai@csus.edu>
2018-12-05 16:40:21 -0800

    Process Stub

---
+++ Process.java
@@ -0,0 +1,19 @@
+//SubPanel for processing text
+import java.awt.*;
+import javax.swing.*;
+
+public class Process extends JPanel
+{
+    //Constructor
+   public LoadPanel()
+   {
+      super();
+      setLayout();
+   }
+   
+   //Set layout of the load panel
+   private void setLayout()
+   {
+   }
+
+}
